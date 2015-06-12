694a554efc56e60afaa433e7aa34775ee7fdffab
Tobias Baum <toby-b@hotmail.de>
2015-06-12 11:44:51 +0200

    In Arbeit Beispiel-Klassen wurden gelöscht zur besseren Übersicht, Websocket-Klasse namens "ATPSWEndpoint" wurde erstellt, diese Klasse soll zur Kommunikation mit dem Frontend dienen. Per JavaScript kann das Frontend Nachrichten an diese Klasse senden, die per onMessage Funktion abgerufen werden und dann je nach empfangener Nachricht Code ausgeführt werden kann. Die onMessage Funktion soll (so die Idee) dann einen ClientThread öffnen, in dem je nach Nachricht vom Frontend db-Befehle ausgeführt werden und per return ans Frontend zurückgegeben werden.

--- Buschfunk/src/buschfunkpackage/Server2.java
+++ Buschfunk/src/buschfunkpackage/Server2.java
@@ -18,6 +18,12 @@ import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
 
 /*
  * The server that can be run both as a console application or a GUI
@@ -66,7 +72,7 @@ public class Server2 {
 
 
 
-	public void start() {
+	public void start() throws IOException, SQLException {
 		keepGoing = true;
 		/* create socket server and wait for connection requests */
 		try 
@@ -82,21 +88,18 @@ public class Server2 {
 				if(!keepGoing)
 					break;
 				
-				// HIER wollen wir die Daten von der Webseite lesen
-				//username= Eingabe von Webseite
-				//ip = Eingabe von Webseite
-				// usw.
+				// !!!!! Hier müssen wir nun irgendwie sicherstellen, dass auf Funktion onMessage gewartet wird
 				
 				
-				ClientThread t = new ClientThread(socket, nameofharry, ipofharry, longitudeofharry, latitudeofharry, c);  //muss noch auf generische Bezeichnungen geändert werden (z.B. username statt nameofharry)
-				al.add(t);									// save it in the ArrayList
-				t.start();									// startet den Threat
+			//	ClientThread t = new ClientThread(socket, nameofharry, ipofharry, longitudeofharry, latitudeofharry, c);  //muss noch auf generische Bezeichnungen geändert werden (z.B. username statt nameofharry)
+				// getAl().add(t);									// save it in the ArrayList
+				// t.start();									// startet den Threat
 			}
-			// I was asked to stop
-			try {											// falls gestoppt, werden alle Threats geschlossen
+			// falls gestoppt, werden alle Threats geschlossen
+			try {											
 				serverSocket.close();
-				for(int i = 0; i < al.size(); ++i) {
-					ClientThread tc = al.get(i);
+				for(int i = 0; i < getAl().size(); ++i) {
+					ClientThread tc = getAl().get(i);
 					try {
 					tc.sInput.close();
 					tc.sOutput.close();
@@ -108,13 +111,19 @@ public class Server2 {
 				}
 			}
 			catch(Exception e) {
-				System.out.println("Exception closing the server and clients: " + e);}
+				System.out.println("Exception closing the server and clients: " + e);
+				}}
 			
+			finally {
+				System.out.println("Ansonsten Fehler, aber warum?");
+			
+				
+			}
 		
 	}		
 	
 	
-	public static void main(String[] args) throws SQLException, NullPointerException {
+	public static void main(String[] args) throws SQLException, NullPointerException, IOException {
 		
 		 Connection c = null;
 	       
@@ -164,47 +173,49 @@ public class Server2 {
 		int id;
 		// the Username of the Client
 		String username;
-		// the only type of message a will receive
-		ChatMessage cm;
 		// the date I connect
 		String date;
 		double longitude;
 		double latitude;
 		String ip;
 		Connection c; // zum testen
+		String message;
 
 		// Konstruktor
-		ClientThread(Socket socket, String username, String ipofharry, double longitudeofharry, double latitudeofharry, Connection c) throws SQLException {
+		List<String> ClientThread(String message, String username, String ipofharry, double longitudeofharry, double latitudeofharry) throws SQLException, IOException {
 			// a unique id
 			id = ++uniqueId;
-			this.socket = socket;
+			//this.socket = socket;
 			this.longitude = longitudeofharry;
 			this.latitude = latitudeofharry;
 			this.ip = ipofharry;
-			this.c = c;
+			this.message = message;
+			//this.c = c;
 			
+			if(message=="Connect") {
+				
+				// boolean checkit = checkname(nameofharry, c);
+				boolean checkit = true; // zum testen
+				
+				if(checkit == true){
+				harrysliste = dbconnectTobi.create(nameofharry, ipofharry, latitudeofharry, longitudeofharry, c);
+				return harrysliste; // Hier geben wir die Userliste aus, muss allerdings noch konkret an Frontend gesendet werden
+				}
+				
+			} else if( message=="Update") {
+				
+				harrysliste2 = dbconnectTobi.update(nameofharry, ipofharry2, latitudeofharry2, longitudeofharry2, c);
+				return harrysliste2;
+				
+			} else if( message=="Logoff") {
+				
+				logoff = dbconnectTobi.logoff(nameofharry, c);
+				System.out.println(logoff);
+				
+			} 
+			return null;
 			
-			
-			// boolean checkit = checkname(nameofharry, c);
-			boolean checkit = true; // zum testen
-		       
-		     if(checkit == true){
-		  	   harrysliste = dbconnectTobi.create(nameofharry, ipofharry, latitudeofharry, longitudeofharry, c);
-		   	   System.out.println(harrysliste); // Hier geben wir die Userliste aus
-		     }
-		     
-		     while(true){
-		    	   if("Frontend drückt Status Aktualisieren"){
-			    	   harrysliste2 = dbconnectTobi.update(nameofharry, ipofharry2, latitudeofharry2, longitudeofharry2, c);
-			    	   System.out.println(harrysliste2);
-		    	   }	
-	    	   
-		    	   if("Frontend drückt Ausloggen oder schließt das Fenster etc"){
-			    	   
-			    	   logoff = dbconnectTobi.logoff(nameofharry, c);
-			    	   System.out.println(logoff);
-			    	   socket.close();
-		    	   }
+		
 		     }
 		} // Ende von Konstruktor ClientThread
 		
@@ -226,7 +237,20 @@ public class Server2 {
 			}
 			catch (Exception e) {}
 		}
-	}
+
+
+
+		public static ArrayList<ClientThread> getAl() {
+			return al;
+		}
+
+
+
+		public void setAl(ArrayList<ClientThread> al) {
+			this.al = al;
+		}
+		
+		
 }
 
 
